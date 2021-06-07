12608b71c13497c32e79dd6f6cd6c65a17414299
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-07 14:12:42 -0400

    Correcting minor error with read-flip handler reorganization

--- src/flips/flips.router.js
+++ src/flips/flips.router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('./flips.controller');
 
 router.route('/').get(controller.list).post(controller.create);
+router.route('/:flipId').get(controller.read);
 
 module.exports = router;
