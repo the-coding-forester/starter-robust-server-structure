4c227d03b4da14f6c090000b3726bac0ca9956eb
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-07 13:57:13 -0400

    Moved POST/flips route handler and validation middleware out of app.js

--- src/flips/flips.controller.js
+++ src/flips/flips.controller.js
@@ -4,6 +4,37 @@ function list(req, res) {
   res.json({ data: flips });
 }
 
+const counts = require('../data/counts-data');
+
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+function bodyHasResultProperty(req, res, next) {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "A 'result' property is required.",
+  });
+}
+
+function create(req, res) {
+  const { data: { result } = {} } = req.body;
+  const newFlip = {
+    id: ++lastFlipId, // Increment last id then assign as the current ID
+    result,
+  };
+  flips.push(newFlip);
+  counts[result] = counts[result] + 1;
+  res.status(201).json({ data: newFlip });
+}
+
+module.exports = {
+  create: [bodyHasResultProperty, create],
+  list,
+};
+
 module.exports = {
   list,
 };
