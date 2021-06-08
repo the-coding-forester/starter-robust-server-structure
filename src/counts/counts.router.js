2b5a3716478a3cda6ca12ea33f839872653ae5c3
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-07 22:23:58 -0400

    Added and implemented and all() handler that returns a 405 status code

--- src/counts/counts.router.js
+++ src/counts/counts.router.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-
 const controller = require('./counts.controller');
+const methodNotAllowed = require('../errors/methodNotAllowed');
 
-router.route('/:countId').get(controller.read);
+router.route('/:countId').get(controller.read).all(methodNotAllowed);
 
-router.route('/').get(controller.list);
+router.route('/').get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
