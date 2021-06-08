90e254727f7a7673287c98b5c6e44701d2740338
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-07 22:39:25 -0400

    Minor fixes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const app = express();
 
-const flips = require('./data/flips-data');
-const counts = require('./data/counts-data');
-
-const flipsRouter = require('./flips/flips.router');
 const countsRouter = require('./counts/counts.router');
 const flipsRouter = require('./flips/flips.router');
 
@@ -15,15 +11,15 @@ app.use('/counts', countsRouter);
 app.use('/flips', flipsRouter);
 
 // Not found handler
-app.use((request, response, next) => {
-  next({ status: 404, message: `Not found: ${request.originalUrl}` });
+app.use((req, res, next) => {
+  next({ status: 404, message: `Not found: ${req.originalUrl}` });
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
   const { status = 500, message = 'Something went wrong!' } = error;
-  response.status(status).json({ error: message });
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
